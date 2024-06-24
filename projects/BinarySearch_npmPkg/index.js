bd336987a391cdf8bc6ac84b3f1fc27e582c468a
Avinash01 <avinash.m1302@gmail.com>
2024-06-24 22:53:05 +0530

    added binary search pkg

---
+++ projects/BinarySearch_npmPkg/index.js
@@ -0,0 +1,18 @@
+const binarySearch = () => {
+  let low = 0;
+  let high = array.length - 1;
+  while(low < high){
+    let mid = parseInt((low + high) / 2);
+    let guess = array[mid];
+    if(guess === target){
+      return mid;
+    } else if (guess > target){
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+}
+
+module.export = binarySearch;
